26b4c337c9dc0c4f217dac4cb83542e7773a9324
GabrielHGomez <beto.mez008@gmail.com>
2026-04-16 20:26:44 -0500

    this is alright I will continue writing code but this is a good backup spot just incase things get alittle crazy

--- src/common.hpp
+++ src/common.hpp
@@ -1,5 +1,33 @@
 #pragma once
 
-void readImage(){
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <string>
 
-}
+template<typename T>
+class SharedMemory{
+public:
+  SharedMemory(std::string& name, bool create = false){
+    if(create){
+	//create shared memory
+    }
+    else{
+	//create reader I think open_only is a possiblity
+    }
+  }
+
+  ~SharedMemory() = default;
+
+  // I will need to add read and write function
+
+
+private:
+  std::string _name;
+  boost::interprocess::shared_memory_object _shm;
+  boost::interprocess::mapped_region _shm;
+  bool _owner = false;
+
+};
+
+
+
--- test/reader.cpp
+++ test/reader.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-  #include "stb_image.h"
 
 int main(){
   return 0;
