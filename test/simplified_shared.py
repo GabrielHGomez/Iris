05d57c00df38f38096f6858da1a2de097d7b7533
GabrielHGomez <beto.mez008@gmail.com>
2026-05-04 22:07:08 -0500

    basic python example. man I really need to fix writer #-#

---
+++ test/simplified_shared.py
@@ -0,0 +1,16 @@
+import mmap
+import struct
+import sys
+import time
+
+FORMAT = "<32s i 2i f"   
+SIZE = struct.calcsize(FORMAT)
+
+name = sys.argv[1] if len(sys.argv) > 1 else "person"
+
+with open(f"/dev/shm/Person_info", "rb") as f:
+    with mmap.mmap(f.fileno(), SIZE, prot=mmap.PROT_READ) as mm:
+        while True:
+            name_b, age, h0, h1, weight = struct.unpack(FORMAT, mm[:SIZE])
+            print(name_b.rstrip(b'\x00').decode(), age, (h0, h1), weight)
+            time.sleep(0.5)
